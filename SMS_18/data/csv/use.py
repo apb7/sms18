ad53fe12861a578a73b61d4dc19e3d5705e13779
Apurv-Bajaj <apurvabajaj007@gmail.com>
2018-02-21 04:04:14 +0530

    Algo added

--- SMS_18/data/csv/use.py
+++ SMS_18/data/csv/use.py
@@ -1,5 +1,24 @@
 import json
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SMS_18.settings")
+import django
+django.setup()
+from main.models import Stock
+import time
+import random
 
 with open('day1.json') as jsonfile:
     dictionary=json.load(jsonfile)
     print(dictionary)
+    
+j = 0    
+for i in range(len(dictionary[list(dictionary.keys())[0]])):    
+    for key in dictionary:
+        print(key)
+        stock = Stock.objects.get(product_name__iexact=key)
+        stock.stock_price = dictionary[key][j]
+        stock.save()
+    j += 1
+    time.sleep(5)
+       
